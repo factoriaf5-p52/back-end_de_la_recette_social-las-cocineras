9bf2109cdc9d5b4ed0b232de0e0cab873bec36cc
Sablinita <sablinahare@gmail.com>
2023-01-27 12:00:03 +0100

    fixed route in main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('la-recette');
   const options = new DocumentBuilder().setTitle('api receta with mysql').setDescription('apirest para receta en sql').setVersion('1.0').build();
-  const document = SwaggerModule.createDocument(app,options);
+  const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
